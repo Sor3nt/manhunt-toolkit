386768b8e01d5fd277c7b9a6fdf102eda555baba
ermaccer <40604575+ermaccer@users.noreply.github.com>
2018-08-14 10:30:34 +0200

    (mh1) added sleep and createinventory item

--- src/Service/Compiler/FunctionMap/Manhunt.php
+++ src/Service/Compiler/FunctionMap/Manhunt.php
@@ -31,177 +31,17 @@ class Manhunt {
 
     public static $constants = [
 
-	
-        'HUD_MAP'  => [
-            'offset' => "01000000"
-        ],
-
-        'HUD_MAN'  => [
-            'offset' => "02000000"
-        ],
-
-        'HUD_HEALTH'  => [
-            'offset' => "04000000"
-        ],
-
-
-        'HUD_STAMINA'  => [
-            'offset' => "08000000"
-        ],
-
-        'HUD_INVENTORY'  => [
-            'offset' => "10000000"
-        ],
-
-
-
-        'AISCRIPT_MEDIUMPRIORITY'  => [
-            'offset' => "02000000"
-        ],
-
-        'AISCRIPT_IDLE_STANDANIMS'  => [
-            'offset' => "05000000"
-        ],
-
-        'AISCRIPT_IDLE_WANDERSEARCH'  => [
-            'offset' => "00000000"
-        ],
-
-        'MTT_HOOD_MEDIUM'  => [
-            'offset' => "02000000"
-        ],
-
-        'AISCRIPT_WALKMOVESPEED'  => [
-            'offset' => "01000000"
-        ],
-
-
-        'COL_PLAYER'  => [
-            'offset' => "00020000"
-        ],
-
-
-        'MAP_COLOR_YELLOW'  => [
-            'offset' => "04000000"
-        ],
-
-        'MAP_COLOR_RED'  => [
-            'offset' => "02000000"
-        ],
-
-        'HID_RADAR'  => [
-            'offset' => "02000000"
-        ],
-
-        'MAP_COLOR_LOCATION'  => [
-            'offset' => "14000000"
-        ],
-
-        'MAP_COLOR_HUNTER_IDLE'  => [
-            'offset' => "08000000"
-        ],
-
-        'COL_HUNTER'  => [
-            'offset' => "10000000"
-        ],
-
-        'AISCRIPT_VERYHIGHPRIORITY'  => [
-            'offset' => "00000000"
-        ],
-
-        'AISCRIPT_LOWPRIORITY'  => [
-            'offset' => "03000000"
-        ],
-
-        'AISCRIPT_IDLE_STANDSTILL'  => [
-            'offset' => "02000000"
-        ],
-
-        'COMBATTYPEID_MELEE'  => [
-            'offset' => "00000000"
-        ],
-
-        'MTT_TRAINING'  => [
-            'offset' => "00000000"
-        ],
-
-        'DIFFICULTY_NORMAL'  => [
-            'offset' => "01000000"
-        ],
-
-        'MAP_COLOR_BLUE'  => [
-            'offset' => "06000000"
-        ],
-
-        'COMBATTYPEID_OPEN_MELEE'  => [
-            'offset' => "03000000"
-        ],
-
-
-        'COMBATTYPEID_COVER'  => [
-            'offset' => "02000000"
-        ],
-
-        'AISCRIPT_GRAPHLINK_ALLOW_NOTHING'  => [
-            'offset' => "00000000"
-        ],
-
-        'HID_ALL_PLAYER_ITEMS'  => [
-            'offset' => "2c010000"
-        ],
-
-        'AISCRIPT_GRAPHLINK_ALLOW_EVERYTHING' => [
-            "offset" => "03000000"
-        ],
-
-        'AISCRIPT_IDLE_PATROL' => [
-            "offset" => "01000000"
-        ],
-
-        'AISCRIPT_HIGHPRIORITY' => [
-            "offset" => "01000000"
-        ],
-
-        'MOVER_STOPPED' => [
-            "offset" => "00000000"
-        ],
-
-        'MOVER_FORWARD' => [
-            "offset" => "01000000"
-        ],
-
-        'AISCRIPT_RUNMOVESPEED' => [
-            "offset" => "00000000"
-        ],
-
-        'DOOR_OPEN' => [
-            "offset" => "00000000"
-        ],
-
-        'DOOR_OPENING' => [
-            "offset" => "01000000"
-        ],
-
-        'DOOR_CLOSED' => [
-            "offset" => "02000000"
-        ],
-
-        'DOOR_CLOSING' => [
-            "offset" => "03000000"
-        ],
-
-        'CT_G_FIRST_AID' => [
-            "offset" => "09000000"
-        ],
-
-        'CT_SYRINGE' => [
-            "offset" => "71000000"
-        ],
 
     ];
 
     public static $functions = [
 
+	
+	        'sleep'  => [
+            'name'      =>  'Sleep',	
+            'offset' => "6a000000"
+        ],     
+        
          'settimer'  => [
             'name'      =>  'SetTimer',
             'offset' => "ce020000",
@@ -307,6 +147,10 @@ class Manhunt {
             'offset' => "3e020000",
             'params'    =>  [ 'String'],
             'desc'      =>  ''
+        ],
+            'createinventoryitem'  => [
+            'name'      =>  'CreateInventoryItem',
+            'offset' => "b9000000",
         ],
     ];
 
