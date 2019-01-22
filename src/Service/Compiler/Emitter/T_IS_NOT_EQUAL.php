76432dc18d5cb9ce4e5b6afd24ecc29e7e0f382b
sor3nt@gmail.com <hyp3rdaewoo>
2019-01-22 19:29:15 +0100

    implementing line debugging mode for T_IS_NOT_EQUAL

--- src/Service/Compiler/Emitter/T_IS_NOT_EQUAL.php
+++ src/Service/Compiler/Emitter/T_IS_NOT_EQUAL.php
@@ -6,7 +6,7 @@ class T_IS_NOT_EQUAL {
     static public function map( $node, \Closure $getLine, \Closure $emitter, $data ){
 
         return [
-            $getLine('40000000')
+            $getLine('40000000', false, '[T_IS_NOT_EQUAL] map')
         ];
 
 
