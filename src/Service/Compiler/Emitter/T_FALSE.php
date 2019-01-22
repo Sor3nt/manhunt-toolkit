6f7439fa0ed285daae40e2aa63a09450afe20b5c
sor3nt@gmail.com <hyp3rdaewoo>
2019-01-22 10:07:13 +0100

    implementing line debugging mode for T_FALSE

--- src/Service/Compiler/Emitter/T_FALSE.php
+++ src/Service/Compiler/Emitter/T_FALSE.php
@@ -7,10 +7,12 @@ use App\Service\Helper;
 class T_FALSE {
 
     static public function map( $node, \Closure $getLine, \Closure $emitter, $data ){
+        $debugMsg = sprintf('[T_FALSE] map ');
+
         return [
-            $getLine('12000000'),
-            $getLine('01000000'),
-            $getLine(Helper::fromIntToHex( 0 ))
+            $getLine('12000000', false, $debugMsg),
+            $getLine('01000000', false, $debugMsg),
+            $getLine(Helper::fromIntToHex( 0 ), false, $debugMsg . ' offset 0')
         ];
 
     }
