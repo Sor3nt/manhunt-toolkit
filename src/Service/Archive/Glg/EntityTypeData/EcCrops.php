34abe729adcb22bfa1b3d1a5122ce0f7513fa645
sor3nt@gmail.com <hyp3rdaewoo>
2019-09-28 19:23:32 +0200

    add missed ec class

---
+++ src/Service/Archive/Glg/EntityTypeData/EcCrops.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Service\Archive\Glg\EntityTypeData;
+
+use App\MHT;
+
+class EcCrops extends Ec {
+
+    public $class           = MHT::EC_CROPS;
+    public $name            = null;
+
+
+}
