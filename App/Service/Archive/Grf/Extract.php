864e0451d86f05fffed35feeefe9347ac7b549fc
sor3nt@gmail.com <hyp3rdaewoo>
2020-02-10 17:07:05 +0100

    add grf mh1 support

--- App/Service/Archive/Grf/Extract.php
+++ App/Service/Archive/Grf/Extract.php
@@ -9,25 +9,37 @@ class Extract {
     /** @var  NBinary */
     private $binary;
 
+    private $game;
+
     public function get( NBinary $binary, $game ){
         $this->binary = $binary;
 
 
         $fourCC = $this->binary->consume(4, NBinary::BINARY);
-        $const = $this->binary->consume(4, NBinary::INT_32);
+
+        if ($fourCC === "GNIA"){
+            $game = MHT::GAME_MANHUNT_2;
+            $const = $this->binary->consume(4, NBinary::INT_32);
+        }else{
+            $game = MHT::GAME_MANHUNT;
+            $this->binary->current = 0;
+        }
+
+        $this->game = $game;
 
         $positions = $this->parsePositions();
+
         $table1 = $this->parseTabel1();
         $table2Names = $this->parseTabel2Names();
 
         $results = [];
-//        foreach ($table1 as $entry) {
-//            foreach ($entry['entries'] as &$positionIndex) {
-//                $positionIndex = $positions[$positionIndex]['position'];
-//            }
-//
-//            $results[ 'table1/' . $entry['name'] . '.json' ] = $entry;
-//        }
+        foreach ($table1 as $entry) {
+            foreach ($entry['entries'] as &$positionIndex) {
+                $positionIndex = $positions[$positionIndex]['position'];
+            }
+
+            $results[ 'path_' . $entry['name'] . '.json' ] = $entry;
+        }
 
         $positionsByIndex = [];
         foreach ($positions as $position) {
@@ -35,7 +47,7 @@ class Extract {
         }
 
         foreach ($table2Names as $index => $name) {
-            $results[ 'table2/' . $name . '.json' ] = $positionsByIndex[$index];
+            $results[ 'area_' . $name . '.json' ] = $positionsByIndex[$index];
         }
 
         return $results;
@@ -86,34 +98,48 @@ class Extract {
 
             $rotation = $this->binary->consume(4, NBinary::FLOAT_32);
 
-            $commandName = $this->binary->getString();
+            $nodeName = $this->binary->getString();
 
 
             $unknown = $this->parseBlock();
-            if(count($unknown)){
-                die("unknown is not empty !!");
-            }
+//            if(count($unknown)){
+//                die("unknown is not empty !!");
+//            }
 
-            $unknown2 = $this->parseBlock();
+
+            if ($this->game == MHT::GAME_MANHUNT_2){
+                $unknown2 = $this->parseBlock();
+            }
 
             $waypoints = $this->parseWayPointBlock();
 
-            $zero1 = $this->binary->consume(4, NBinary::INT_32);
-            if ($zero1 != 0) die("zero is not zero ...");
-            $zero2 = $this->binary->consume(4, NBinary::INT_32);
-            if ($zero2 != 0) die("zero2 is not zero ...");
+
+            if ($this->game == MHT::GAME_MANHUNT_2) {
+                $zero1 = $this->binary->consume(4, NBinary::INT_32);
+                if ($zero1 != 0) die("zero is not zero ...");
+                $zero2 = $this->binary->consume(4, NBinary::INT_32);
+                if ($zero2 != 0) die("zero2 is not zero ...");
+            }
 
             $entries[] = [
                 'id' => $id,
                 'groupIndex' => $groupIndex,
-                'commandName' => $commandName,
+                'nodeName' => $nodeName,
                 'position' => $position,
-                'rotation' => $rotation,
+                'speed' => $rotation,
                 'unknown' => $unknown,
-                'unknown2' => $unknown2,
-                'waypoints' => $waypoints
+//                'unknown2' => $unknown2,
+                'entries' => $waypoints
             ];
         }
+
+        usort($entries, function ($a, $b){
+            return $a['id'] > $b['id'];
+        });
+//
+//        sort($this->tmp);
+//        var_dump($this->tmp);
+//        exit;
 
         return $entries;
     }
@@ -130,6 +156,8 @@ class Extract {
     }
 
 
+    private $tmp = [];
+
     private function parseWayPointBlock(){
 
         $count = $this->binary->consume(4, NBinary::INT_32);
@@ -137,16 +165,25 @@ class Extract {
         $result = [];
         for($x = 0; $x < $count; $x++){
 
-            $linkId = $this->binary->consume(4, NBinary::INT_32);
-            $unknownNumber = $this->binary->consume(4, NBinary::INT_32);
+            $linkId1 = $this->binary->consume(4, NBinary::INT_32);
+//            $linkId1 = $this->binary->consume(2, NBinary::INT_16);
+//            $linkId2 = $this->binary->consume(2, NBinary::INT_16);
+            $type = $this->binary->consume(4, NBinary::INT_32);
+
+//            $this->tmp[]  = $linkId;
+            $entry = [
+                'linkId' => $linkId1,
+//                'linkId2' => $linkId2,
+                'type' => $type
+            ];
 
             $unknown3 = $this->parseBlock();
 
-            $result[] = [
-                'linkId' => $linkId,
-                'unknownNumber' => $unknownNumber,
-                'unknown3' => $unknown3
-            ];
+            if (count($unknown3)){
+                $entry['unknown3'] = $unknown3;
+            }
+
+            $result[] = $entry;
         }
 
         return $result;
