6c1a8d71b372cf3ecedebdf4c8ae125326b55d1c
sor3nt@gmail.com <hyp3rdaewoo>
2020-11-14 22:33:21 +0100

    dev... not ready for usage

--- Application/mht.php
+++ Application/mht.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('memory_limit','-1');
 
-require_once __DIR__ . '/Application/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $options = [];
 foreach ($argv as $index => $argument) {
@@ -30,24 +30,56 @@ if (isset($argv[1])){
         case 'unpack':
         case 'extract':
             echo "\n";
-            include __DIR__ . '/Application/App/Commands/unpack.php';
+            include __DIR__ . '/App/Commands/unpack.php';
             break;
 
         case 'pack':
         case 'build':
             echo "\n";
-            include __DIR__ . '/Application/App/Commands/pack.php';
+            include __DIR__ . '/App/Commands/pack.php';
 
             break;
         case 'patch':
             echo "Legend: U=unpack B=build S=skip R=replace A=append\n";
             echo "\n";
-            include __DIR__ . '/Application/App/Commands/patch.php';
+            include __DIR__ . '/App/Commands/patch.php';
 
             break;
         case 'compare':
             echo "\n";
-            include __DIR__ . '/Application/App/Commands/compare.php';
+            include __DIR__ . '/App/Commands/compare.php';
+
+            break;
+
+        case 'install':
+            echo "\n";
+
+var_dump(realpath(__FILE__));
+            echo "Installing MHT...";
+            //Windows
+            if(strcasecmp(substr(PHP_OS, 0, 3), 'WIN') == 0){
+
+                file_put_contents('mht.bat', PHP_BINARY . ' ' . __DIR__ . '\\mht.phar');
+
+                system(sprintf("setx path \"%path%;%s\"", __DIR__));
+
+            //Linux/Mac
+            }else{
+
+                $cmd = sprintf(
+                    "%s %s%s%s",
+                    PHP_BINARY,
+                    getcwd(),
+                    DIRECTORY_SEPARATOR,
+                    'mht.phar $@'
+                );
+
+                file_put_contents("/usr/local/bin/mht", $cmd);
+                chmod("/usr/local/bin/mht", 0777);
+
+            }
+
+            echo "OK\n";
 
             break;
 
@@ -63,6 +95,7 @@ if (isset($argv[1])){
 function help(){
     echo "Usage: mht <command>\n";
     echo "-------\n";
+    echo "install\t\t\tInstall MHT globally\n";
     echo "unpack [extract]\tExtract a Manhunt File\n";
     echo "pack [build]\t\tBuild a Manhunt File\n";
     echo "compare\t\t\tCompares 2 Folder, generate CSVs\n";
---
+++ build.php
@@ -0,0 +1,42 @@
+<?php
+
+$pharFile = 'mht.phar';
+
+// clean up
+if (file_exists($pharFile))
+{
+    unlink($pharFile);
+}
+
+if (file_exists($pharFile . '.gz'))
+{
+    unlink($pharFile . '.gz');
+}
+
+// create phar
+$phar = new Phar($pharFile);
+
+// start buffering. Mandatory to modify stub to add shebang
+$phar->startBuffering();
+
+// Create the default stub from main.php entrypoint
+$defaultStub = $phar->createDefaultStub('mht.php');
+
+// Add the rest of the apps files
+$phar->buildFromDirectory(__DIR__ . '/Application');
+
+// Customize the stub to add the shebang
+$stub = "#!/usr/bin/php \n" . $defaultStub;
+
+// Add the stub
+$phar->setStub($stub);
+
+$phar->stopBuffering();
+
+// plus - compressing it into gzip
+$phar->compressFiles(Phar::GZ);
+
+# Make the file executable
+chmod(__DIR__ . '/' . $pharFile, 0770);
+
+echo "$pharFile successfully created" . PHP_EOL;
