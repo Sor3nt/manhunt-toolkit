d0699a6d63a3c6827bb61ea0b3c08fd271efcf4b
sor3nt@gmail.com <hyp3rdaewoo>
2021-05-16 20:53:14 +0200

    rw cleanup

--- Application/Ide/src/Library/Renderware/Renderware.js
+++ Application/Ide/src/Library/Renderware/Renderware.js
@@ -7,6 +7,8 @@ Renderware = (function () {
 
     var self = {
         getMap: function(nBinary, level){
+            nBinary.setCurrent(0);
+
             let tree = RW.parser(nBinary).parse();
             return RW.convert.map(tree, level);
         },
@@ -16,7 +18,6 @@ Renderware = (function () {
 
             let tree = RW.parser(nBinary).parse();
             return RW.convert.model(tree);
-
         }
     };
 
